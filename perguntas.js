b844f6d8d0d626b9431a54cafa1b72fd63deeaae
Geovanna Rodrigues <0000109380905Xsp@al.educacao.sp.gov.br>
2024-11-26 07:31:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital  do Brasil?',
+    'A capital do Brasil é Brasília' )
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil foi descoberto?',
+    'O Brasil foi descoberto em 1500'
+)
+
+criaCartao(
+    'Esportes',
+    'Em que ano o Brasil foi pentacampeão da Copa do Mundo?',
+    'O Brasil foi penta na Copa de 2002' 
+)
+
+criaCartao(
+    'Geopolítica',
+    'Qual grupo de países emergentes o Brasil participa?',
+    'Os BRICS é o grupo de países emergentes que o Brasil participa'
+)
